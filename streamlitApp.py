28f1bcfc41e221d173bc5e21b76e85ac3f8be934
iqrasanaullah <sanaiqra087@gmail.com>
2024-09-05 10:11:39 +0000

    fixed the bugs in the app

--- streamlitApp.py
+++ streamlitApp.py
@@ -6,18 +6,18 @@ import streamlit as st
 # load the model to made live
 model = joblib.load("liveModelV1.pk1")
 
-data = pd.read_csv('mobile_price_range_data.csv (5).csv')
+data = pd.read_csv('mobile_price_range_data (5).csv')
 x = data.iloc[:,:-1]
 y = data.iloc[:,-1]
 
-x_train,x_test,y_train,y_test = train_test_split(x,y,tset_size=0.2, random_state=42)
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2, random_state=42)
 # make prediction for x_test set
 y_pred = model.predict(x_test)
 
 #calculate accuracy
 accuracy = accuracy_score(y_test,y_pred)
 
-st.tittle("Model Accuracy and Real-Time prediction")
+st.title("Model Accuracy and Real-Time prediction")
 
 #Display Accuracy
 st.write(f"Model{accuracy}")
@@ -26,7 +26,7 @@ st.write(f"Model{accuracy}")
 st.header("Real-Time prediction")
 input_data = []
 for col in x_test.columns:
-    input_value = st.number_input(f'Input for feature{col}',value='')
+    input_value = st.number_input(f'Input for feature{col}',value=0)
     input_data.append(input_value)
 # convert input data to data frame
 input_df = pd.DataFrame([input_data],columns=x_test.columns)  
